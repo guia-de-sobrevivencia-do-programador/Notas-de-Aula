6daa940c43306ab81c3bd80ff5a158c95aeab8b8
nkzren <renan.nakazawa0@gmail.com>
2022-03-18 22:04:12 -0300

    adiciona testes de unidade

--- 7 - Metaprogramação/tarefa/src/calculadora.test.js
+++ 7 - Metaprogramação/tarefa/src/calculadora.test.js
@@ -1,3 +1,11 @@
+const { pow } = require('mathjs')
+
+jest.mock('mathjs', () =>  {
+    return {
+        pow: jest.fn()
+    }
+})
+
 const calculadora = require('./calculadora')
 
 test('deve somar dois numeros', () => {
@@ -5,3 +13,33 @@ test('deve somar dois numeros', () => {
 
   expect(resultado).toBe(3)
 })
+
+test('deve subtrair dois numeros', () => {
+  const resultado = calculadora.calcula(1, 2, 'sub')
+
+  expect(resultado).toBe(-1)
+})
+
+test('deve multiplicar dois numeros', () => {
+  const resultado = calculadora.calcula(3, 2, 'mult')
+
+  expect(resultado).toBe(6)
+})
+
+test('deve dividir dois numeros', () => {
+  const resultado = calculadora.calcula(10, 2, 'div')
+
+  expect(resultado).toBe(5)
+})
+
+test('deve elevar um numero pelo outro', () => {
+  const resultado = calculadora.calcula(2, 2, 'pot')
+
+  expect(pow).toHaveBeenCalledWith(2,2)
+})
+
+test('deve retornar um erro numa operacao invalida', () => {
+  expect(() => calculadora.calcula(2, 2, 'operacao_invalida')).toThrow(Error("Operacao invalida"))
+})
+
+
